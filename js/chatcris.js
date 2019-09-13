0df20aef8f8dd19d3e2746b128ce1ae2f1fb67c0
Ricardo Cuono <ricardo@tecpay.com.br>
2019-09-13 10:44:26 -0300

    Update chatcris.js

--- js/chatcris.js
+++ js/chatcris.js
@@ -3,7 +3,7 @@ function perguntaPraCris(){
     var check = input.includes("ajuda");
     var resposta;
     var i = 0;
-    var speed = 10;
+    var speed = 30;
 
     switch (input) {
       case "":
